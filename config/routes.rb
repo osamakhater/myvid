ea1b9fabccfe37974926fa79ac911f1ff790163c
Osama Khater <osamakhater5@gmail.com>
2016-07-03 14:03:47 +0300

    my second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   resources :videos
   get 'pages/about'
 
   get 'pages/contact'
+  root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
